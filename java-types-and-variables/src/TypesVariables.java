b3cd40f73e02cb7a6d85c2dc5ec146dbaa1985a2
WaterWhirl <waterwhirl@hotmail.com>
2023-01-10 23:39:45 -0300

    feat: learn about variable and Constant

--- java-types-and-variables/src/TypesVariables.java
+++ java-types-and-variables/src/TypesVariables.java
@@ -1,5 +1,17 @@
 public class TypesVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double minimumSalary = 2500.50;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        int number = 1;
+
+        number = 10;
+
+        System.out.println(number);
+
+        final double PI_VALOR = 3.14;
     }
 }
