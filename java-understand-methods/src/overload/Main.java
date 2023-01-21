1d8890151fa9843fee69e48b7aaa8cbc491e4ea9
WaterWhirl <waterwhirl@hotmail.com>
2023-01-21 20:01:21 -0300

    feat: learning about overload

---
+++ java-understand-methods/src/overload/Main.java
@@ -0,0 +1,13 @@
+package overload;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Quadrilateral
+        System.out.println("Quadrilateral exercise");
+        Quadrilateral.area(3);
+        Quadrilateral.area(5, 5);
+        Quadrilateral.area(7, 8, 9);
+        Quadrilateral.area(5f, 5f);
+    }
+}
---
+++ java-understand-methods/src/overload/Quadrilateral.java
@@ -0,0 +1,30 @@
+package overload;
+
+import javax.print.event.PrintServiceAttributeListener;
+
+/**
+ * Exercise class 2 about methods.
+ */
+
+public class Quadrilateral {
+    
+    public static void area(double side) {
+
+        System.out.println("Square area: " + side * side);
+    }
+
+    public static void area(double side1, double side2) {
+
+        System.out.println("Rectangle area: " + side1 * side2);
+    }
+
+    public static void area(double majorBase, double minorBase, double height) {
+
+        System.out.println("Triangle area: " + ((majorBase * minorBase) * height) / 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println("Losangle area: " + (diagonal1 * diagonal2) / 2);
+    }
+}
